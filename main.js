31eb89d8508c8bf58b8796d57f1250d1e0fe7ee8
iamsynthetic <synthetic_samurai@hotmail.com>
2025-06-07 23:35:18 -0400

    added replay button

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 //////  INIT  ////////
-
+// alert("animation started");
+TweenMax.set(thereplay, { visibility: "hidden" });
 container.addEventListener("mouseover", ctarollover);
 container.addEventListener("mouseout", ctarolloff);
 thereplay.addEventListener("click", replaybanner);
 
 function replaybanner() {
-  replayad();
+  startAd();
 }
 function ctarollover() {
   TweenMax.to(cta, 0.2, {
@@ -32,56 +33,12 @@ hideElement = function (e) {
   e.style.visibility = "hidden";
 };
 
-function replayad() {
-  showElement(txt1);
-  TweenMax.set(txt1, { y: "0px", opacity: 1, scale: 1 });
-  showElement(txt2a);
-  TweenMax.set(txt2a, { y: "0px", opacity: 1, scale: 1 });
-  showElement(txt2b);
-  TweenMax.set(txt2b, { y: "0px", opacity: 1, scale: 1 });
-  showElement(txt3);
-  TweenMax.set(txt3, { y: "0px", opacity: 1, scale: 1 });
-  showElement(dog);
-  TweenMax.set(dog, { y: "0px", opacity: 1, scale: 1 });
-  showElement(txt4a);
-  TweenMax.set(txt4a, { y: "0px", opacity: 1, scale: 1 });
-  showElement(txt4b);
-  TweenMax.set(txt4b, { y: "0px", opacity: 1, scale: 1 });
-  showElement(txt4c);
-  TweenMax.set(txt4c, { y: "0px", opacity: 1, scale: 1 });
-  showElement(finalframebg);
-  TweenMax.set(finalframebg, { y: "0px", opacity: 1, scale: 1 });
-  showElement(txt5a);
-  showElement(txt5b);
-  showElement(txt5c);
-  showElement(txt5d);
-  showElement(cta);
-  TweenMax.set(".halfsize", { transformOrigin: "left top", scale: 0.5 });
-  TweenMax.set(".regsize", { transformOrigin: "left top", scale: 1 });
-  TweenMax.set(".lgsize", { transformOrigin: "left top", scale: 2 });
-
-  // set up initial positions
-  hideElement(txt1);
-  hideElement(txt2a);
-  hideElement(txt2b);
-  hideElement(txt3);
-  hideElement(dog);
-  hideElement(txt4a);
-  hideElement(txt4b);
-  hideElement(txt4c);
-  hideElement(finalframebg);
-  hideElement(txt5a);
-  hideElement(txt5b);
-  hideElement(txt5c);
-  hideElement(txt5d);
-  hideElement(cta);
-  console.log("daaaamn 22");
-  TweenMax.delayedCall(0, intro);
-  // TweenMax.delayedCall(0, startAd);
-}
 function startAd() {
+  console.log("started");
   startAd2();
   // size setup
+
+  TweenMax.set(thereplay, { visibility: "hidden" });
   TweenMax.set(".halfsize", { transformOrigin: "left top", scale: 0.5 });
   TweenMax.set(".regsize", { transformOrigin: "left top", scale: 1 });
   TweenMax.set(".lgsize", { transformOrigin: "left top", scale: 2 });
@@ -101,7 +58,7 @@ function startAd() {
   hideElement(txt5c);
   hideElement(txt5d);
   hideElement(cta);
-  console.log("daaaamn 22");
+  hideElement(cover);
   TweenMax.delayedCall(0, intro);
 }
 
@@ -115,9 +72,10 @@ revealAd = function () {
 };
 
 frame1 = function () {
+  TweenMax.set(txt1, { top: 103, left: 16, y: 0, opacity: 1, scale: 1 });
   showElement(txt1);
   TweenMax.from(txt1, 1, {
-    y: 300,
+    top: 300,
     ease: "Back.easeInOut",
     onComplete: function () {
       frame2();
@@ -126,6 +84,8 @@ frame1 = function () {
 };
 
 frame2 = function () {
+  TweenMax.set(txt2a, { top: 125, left: 64, opacity: 1, scale: 1 });
+  TweenMax.set(txt2b, { top: 161, left: 78, opacity: 1, scale: 1 });
   let amountofdelay = 1.5;
   showElement(txt2a);
   showElement(txt2b);
@@ -150,6 +110,8 @@ frame2 = function () {
 };
 
 frame3 = function () {
+  TweenMax.set(txt3, { top: 125, left: 64, opacity: 1, scale: 1 });
+  TweenMax.set(dog, { top: 0, left: 0, opacity: 1, scale: 1 });
   let amountofdelay = 1.5;
 
   showElement(dog);
@@ -192,6 +154,11 @@ frame3 = function () {
 
 frame5 = function () {
   let amountofdelay = 1.5;
+
+  TweenMax.set(txt4a, { top: 21, left: 102, opacity: 1, scale: 1 });
+  TweenMax.set(txt4b, { top: 9, left: 193, opacity: 1, scale: 1 });
+  TweenMax.set(txt4c, { top: 47, left: 240, opacity: 1, scale: 1 });
+
   showElement(txt4a);
   showElement(txt4b);
   showElement(txt4c);
@@ -254,6 +221,14 @@ frame5 = function () {
 
 frame6 = function () {
   let amountofdelay = 0;
+
+  TweenMax.set(finalframebg, { top: 0, left: 0, scale: 1, opacity: 1 });
+  TweenMax.set(txt5a, { top: 58, left: 12, scale: 1, opacity: 1 });
+  TweenMax.set(txt5b, { top: 111, left: 15, scale: 1, opacity: 1 });
+  TweenMax.set(txt5c, { top: 180, left: 18, scale: 1, opacity: 1 });
+  TweenMax.set(txt5d, { top: 220, left: 87, scale: 1, opacity: 1 });
+  TweenMax.set(cta, { top: 20, left: 182, scale: 1, opacity: 1 });
+
   showElement(finalframebg);
   showElement(txt5a);
   showElement(txt5b);
@@ -300,6 +275,9 @@ frame6 = function () {
     opacity: 0,
     ease: "Quint.easeOut",
     delay: amountofdelay + 0.5,
+    onComplete: function () {
+      TweenMax.set(thereplay, { visibility: "visible" });
+    },
   });
 };
 
@@ -308,9 +286,10 @@ frame6 = function () {
 container2.addEventListener("mouseover", ctarollover);
 container2.addEventListener("mouseout", ctarolloff);
 thereplay2.addEventListener("click", replaybanner2);
+TweenMax.set(thereplay2, { visibility: "hidden" });
 
 function replaybanner2() {
-  replayad2();
+  startAd2();
 }
 
 function ctarollover() {
@@ -338,57 +317,8 @@ hideElement = function (e) {
   e.style.visibility = "hidden";
 };
 
-function replayad2() {
-  showElement(txt1);
-  TweenMax.set(txt1, { y: "0px", opacity: 1, scale: 1 });
-  showElement(txt2a);
-  TweenMax.set(txt2a, { y: "0px", opacity: 1, scale: 1 });
-  showElement(txt2b);
-  TweenMax.set(txt2b, { y: "0px", opacity: 1, scale: 1 });
-  showElement(txt3);
-  TweenMax.set(txt3, { y: "0px", opacity: 1, scale: 1 });
-  showElement(dog);
-  TweenMax.set(dog, { y: "0px", opacity: 1, scale: 1 });
-  showElement(txt4a);
-  TweenMax.set(txt4a, { y: "0px", opacity: 1, scale: 1 });
-  showElement(txt4b);
-  TweenMax.set(txt4b, { y: "0px", opacity: 1, scale: 1 });
-  showElement(txt4c);
-  TweenMax.set(txt4c, { y: "0px", opacity: 1, scale: 1 });
-  showElement(finalframebg);
-  TweenMax.set(finalframebg, { y: "0px", opacity: 1, scale: 1 });
-  showElement(txt5a);
-  showElement(txt5b);
-  showElement(txt5c);
-  showElement(txt5d);
-  showElement(cta);
-
-  TweenMax.set(".halfsize", { transformOrigin: "left top", scale: 0.5 });
-  TweenMax.set(".regsize", { transformOrigin: "left top", scale: 1 });
-  TweenMax.set(".lgsize", { transformOrigin: "left top", scale: 2 });
-
-  // set up initial positions
-  hideElement(txtbubble12);
-  hideElement(dots12);
-  hideElement(txtbubble22);
-  hideElement(dots22);
-  hideElement(txtbubble32);
-  hideElement(txtbubble42);
-  hideElement(dog2);
-  hideElement(txt4a2);
-  hideElement(txt4b2);
-  hideElement(txt4c2);
-  hideElement(finalframebg2);
-  hideElement(txt5a2);
-  hideElement(txt5b2);
-  hideElement(txt5c2);
-  hideElement(txt5d2);
-  hideElement(cta2);
-  TweenMax.delayedCall(0, intro2);
-  // TweenMax.delayedCall(0, startAd2);
-}
-
 function startAd2() {
+  TweenMax.set(thereplay2, { visibility: "hidden" });
   // size setup
   TweenMax.set(".halfsize", { transformOrigin: "left top", scale: 0.5 });
   TweenMax.set(".regsize", { transformOrigin: "left top", scale: 1 });
@@ -411,23 +341,25 @@ function startAd2() {
   hideElement(txt5c2);
   hideElement(txt5d2);
   hideElement(cta2);
+  hideElement(cover2);
   TweenMax.delayedCall(0, intro2);
 }
 
-intro2 = function () {
+function intro2() {
+  console.log("intro2");
   TweenMax.delayedCall(0, revealAd2);
   TweenMax.delayedCall(1, frame12);
   TweenMax.delayedCall(2, bluebubbles);
   TweenMax.delayedCall(4.2, creambubbles);
   TweenMax.delayedCall(6.5, bluebubbles);
   TweenMax.delayedCall(8.7, creambubbles);
-};
+}
 
-revealAd2 = function () {
+function revealAd2() {
   TweenMax.to(cover2, 0.5, { alpha: 0 });
-};
+}
 
-bluebubbles = function () {
+function bluebubbles() {
   TweenMax.to(txtdotA32, 0.5, {
     scale: 0,
     ease: "Quint.easeOut",
@@ -442,9 +374,9 @@ bluebubbles = function () {
     delay: -0.5,
     ease: "Quint.easeOut",
   });
-};
+}
 
-creambubbles = function () {
+function creambubbles() {
   TweenMax.to(txtdotB32, 0.5, {
     scale: 0,
     ease: "Quint.easeOut",
@@ -459,11 +391,17 @@ creambubbles = function () {
     delay: -0.5,
     ease: "Quint.easeOut",
   });
-};
+}
 
-frame12 = function () {
+function frame12() {
   showElement(dots12);
   showElement(txtbubble12);
+  TweenMax.set(dots12, { top: 230, left: 14, scale: 1, opacity: 1 });
+  TweenMax.set(txtdotA32, { scale: 1, opacity: 1 });
+  TweenMax.set(txtdotA22, { scale: 1, opacity: 1 });
+  TweenMax.set(txtdotA12, { scale: 1, opacity: 1 });
+  TweenMax.set(txtbubble12, { top: 200, left: 14, y: 0, scale: 1, opacity: 1 });
+
   TweenMax.from(txtdotA32, 0.5, {
     scale: 0,
     ease: "Back.easeIn",
@@ -486,11 +424,24 @@ frame12 = function () {
       frame22();
     },
   });
-};
+}
 
-frame22 = function () {
+function frame22() {
   showElement(txtbubble22);
   showElement(dots22);
+
+  TweenMax.set(dots22, { top: 230, left: 240, scale: 1, opacity: 1 });
+  TweenMax.set(txtdotB32, { scale: 1, opacity: 1 });
+  TweenMax.set(txtdotB22, { scale: 1, opacity: 1 });
+  TweenMax.set(txtdotB12, { scale: 1, opacity: 1 });
+  TweenMax.set(txtbubble22, {
+    top: 170,
+    left: 140,
+    y: 0,
+    scale: 1,
+    opacity: 1,
+  });
+
   let amountofdelay = 1;
   TweenMax.to(txtbubble12, 0, {
     y: "-10",
@@ -538,11 +489,13 @@ frame22 = function () {
       frame32();
     },
   });
-};
+}
 
-frame32 = function () {
+function frame32() {
   showElement(txtbubble32);
   showElement(dots12);
+  TweenMax.set(txtbubble32, { top: 200, left: 14, y: 0, scale: 1, opacity: 1 });
+
   let amountofdelay = 1;
   TweenMax.to(txtbubble12, 0, {
     y: "-80",
@@ -588,11 +541,20 @@ frame32 = function () {
       frame42();
     },
   });
-};
+}
 
-frame42 = function () {
+function frame42() {
   showElement(txtbubble42);
   showElement(dots22);
+
+  TweenMax.set(txtbubble42, {
+    top: 170,
+    left: 140,
+    y: 0,
+    scale: 1,
+    opacity: 1,
+  });
+
   let amountofdelay = 1;
   TweenMax.to(txtbubble12, 0, {
     y: "-140",
@@ -621,11 +583,6 @@ frame42 = function () {
     delay: amountofdelay + -0.4,
     ease: "Back.easeIn",
   });
-  // TweenMax.from(txtdotB3, 0.5, {
-  //   scale: 0,
-  //   delay: amountofdelay,
-  //   ease: "Back.easeIn",
-  // });
   TweenMax.to(txtbubble12, 0, {
     y: "-230",
     delay: amountofdelay + 1,
@@ -646,13 +603,13 @@ frame42 = function () {
       frame52();
     },
   });
-};
+}
 
-frame52 = function () {
+function frame52() {
   let amountofdelay = 1.5;
 
   showElement(dog2);
-  TweenMax.set(dog2, { y: 300 });
+  TweenMax.set(dog2, { top: 0, left: 0, y: 300, opacity: 1, scale: 1 });
 
   TweenMax.to(dog2, 2, {
     y: 115,
@@ -662,13 +619,17 @@ frame52 = function () {
       frame62();
     },
   });
-};
+}
 
-frame62 = function () {
+function frame62() {
   let amountofdelay = 1.5;
   showElement(txt4a2);
   showElement(txt4b2);
   showElement(txt4c2);
+
+  TweenMax.set(txt4a2, { top: 21, left: 102, opacity: 1, scale: 1 });
+  TweenMax.set(txt4b2, { top: 9, left: 193, opacity: 1, scale: 1 });
+  TweenMax.set(txt4c2, { top: 47, left: 240, opacity: 1, scale: 1 });
 
   TweenMax.to(txtbubble12, 0.2, {
     scale: 0.9,
@@ -742,9 +703,9 @@ frame62 = function () {
       frame72();
     },
   });
-};
+}
 
-frame72 = function () {
+function frame72() {
   let amountofdelay = 0;
   showElement(finalframebg2);
   showElement(txt5a2);
@@ -752,6 +713,13 @@ frame72 = function () {
   showElement(txt5c2);
   showElement(txt5d2);
   showElement(cta2);
+
+  TweenMax.set(finalframebg2, { top: 0, left: 0, scale: 1, opacity: 1 });
+  TweenMax.set(txt5a2, { top: 58, left: 12, scale: 1, opacity: 1 });
+  TweenMax.set(txt5b2, { top: 111, left: 15, scale: 1, opacity: 1 });
+  TweenMax.set(txt5c2, { top: 180, left: 18, scale: 1, opacity: 1 });
+  TweenMax.set(txt5d2, { top: 220, left: 87, scale: 1, opacity: 1 });
+  TweenMax.set(cta2, { top: 20, left: 182, scale: 1, opacity: 1 });
 
   TweenMax.from(finalframebg2, 1, {
     y: 300,
@@ -792,5 +760,11 @@ frame72 = function () {
     opacity: 0,
     ease: "Quint.easeOut",
     delay: amountofdelay + 0.5,
+    onComplete: function () {
+      TweenMax.set(thereplay2, { visibility: "visible" });
+    },
   });
-};
+}
+
+startAd();
+// startAd2();
